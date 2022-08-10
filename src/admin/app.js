8907cdde3a9aca64915fb747f15d724ecb0d5c1c
mlsokol <103022498+mlsokol@users.noreply.github.com>
2022-08-10 18:54:56 -0400

    initial commit

---
+++ src/admin/app.js
@@ -0,0 +1,56 @@
+import AuthLogo from './extensions/my-logo.png';
+import MenuLogo from './extensions/logo.png';
+import favicon from './extensions/favicon.ico';
+
+export default {
+  config: {
+    // Replace the Strapi logo in auth (login) views
+    auth: {
+      logo: AuthLogo,
+    },
+   // Replace the favicon
+    head: {
+      favicon: favicon,
+    },
+    // Add a new locale, other than 'en'
+    locales: ['fr', 'de'],
+    // Replace the Strapi logo in the main navigation
+    menu: {
+      logo: MenuLogo,
+    },
+    // Override or extend the theme
+    theme: {
+      colors: {
+        primary100: '#EAEBEA',
+        primary200: '#B5BAB7',
+        primary500: '#777E79',
+        primary600: '#5B5B5D',
+        primary700: '#3a3d3b',
+        danger700: '#CB423D',
+        secondary100:'#eafbd0',
+        secondary200:'#dff9b9',
+        secondary500:'#caf5ab',
+        secondary600:'#b4f05c',
+        secondary700:'#94ea16',
+        buttonPrimary500: '#887750',
+        buttonPrimary600: '#887750'
+        },
+    },
+    // Extend the translations
+    translations: {
+      fr: {
+        'Auth.form.email.label': 'test',
+        Users: 'Utilisateurs',
+        City: 'CITY (FRENCH)',
+        // Customize the label of the Content Manager table.
+        Id: 'ID french',
+      },
+    },
+   // Disable video tutorials
+    tutorials: false,
+   // Disable notifications about new Strapi releases
+    notifications: { release: false },
+  },
+
+  bootstrap() {},
+};
