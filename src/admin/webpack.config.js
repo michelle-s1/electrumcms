c766e930e7a14051865568633f07c96ae9ba39c8
mlsokol <michelle@listenupcreative.com>
2022-08-11 01:29:27 -0400

    webpack

--- src/admin/webpack.config.js
+++ src/admin/webpack.config.js
@@ -7,13 +7,4 @@ module.exports = (config) => {
   // Perform customizations to webpack config
   // Important: return the modified config
   return config;
-  
-  webpack:{(config, { isServer }) => {
-    if (!isServer) {
-        // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
-        config.resolve.fallback = {
-            fs: false
-        }
-    }}
-  }
 };
