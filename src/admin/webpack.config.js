6c57e94c31943376f8ab0314b24e0049d951f9ce
mlsokol <michelle@electrumsupply.com>
2022-08-24 17:19:40 -0400

    rebuild webpack

--- src/admin/webpack.config.js
+++ src/admin/webpack.config.js
@@ -2,12 +2,10 @@
 
 const _ = require('lodash');
 const path = require('path');
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
+/* eslint-disable no-unused-vars */
 module.exports = (config) => {
-  config.plugins.push(new MonacoWebpackPlugin());
+  // this includes the folder ./src/admin/assets with the alias assets
   _.set(config, 'resolve.alias.assets', path.resolve(__dirname, './assets'));
-
-
   return config;
 };
