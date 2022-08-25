bc1a9355f7e709c7bcba8cfa1138b43eb22d1eca
mlsokol <michelle@electrumsupply.com>
2022-08-25 15:13:46 -0400

    locations

---
+++ src/api/warehouse-location/controllers/warehouse-location.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * warehouse-location controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::warehouse-location.warehouse-location');
---
+++ src/api/warehouse-location/routes/warehouse-location.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * warehouse-location router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::warehouse-location.warehouse-location');
