9e20422896f0b283c2ee57e1eddff2703913788f
mlsokol <michelle@electrumsupply.com>
2022-08-25 03:34:53 -0400

    aws

--- config/plugins.js
+++ config/plugins.js
@@ -4,5 +4,23 @@ module.exports = ({ env }) => ({
 	},
     'import-export-entries': {
         enabled: true,
-    }
+    },
+    upload: {
+        config: {
+          provider: 'aws-s3',
+          providerOptions: {
+            accessKeyId: env('ACCESS_KEY_ID'),
+            secretAccessKey: env('SECRET_ACCESS_TOKEN'),
+            region: 'us-east-2',
+            params: {
+              Bucket: 'electrumstrapi',
+            },
+          },
+          actionOptions: {
+            upload: {},
+            uploadStream: {},
+            delete: {},
+          },
+        },
+      },
 });
